766ce915fdedd648895b5b05594f998e0e6104de
FrozenLemonTee <1115306170@qq.com>
2025-01-04 19:51:11 +0800

    feat: Init repo

---
+++ docs/html/search/searchdata.js
@@ -0,0 +1,18 @@
+var indexSectionsWithContent =
+{
+  0: "co多已并文模正简网计",
+  1: "o"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Pages"
+};
+
