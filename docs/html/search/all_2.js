0f9b117fdf563dcd44e85c52fdd7609186ea82ed
FrozenLemonTee <1115306170@qq.com>
2025-01-06 21:06:00 +0800

    docs: Update docs

--- docs/html/search/all_2.js
+++ docs/html/search/all_2.js
@@ -1,16 +1,18 @@
 var searchData=
 [
   ['callbackchecker_0',['callBackChecker',['../classoriginal_1_1call_back_checker.html',1,'original']]],
-  ['chain_1',['chain',['../classoriginal_1_1chain.html',1,'original']]],
-  ['chain_3c_20opts_20_3e_2',['chain&lt; opts &gt;',['../classoriginal_1_1chain.html',1,'original']]],
-  ['chain_3c_20std_3a_3ashared_5fptr_3c_20original_3a_3afilter_3c_20type_20_3e_20_3e_20_3e_3',['chain&lt; std::shared_ptr&lt; original::filter&lt; TYPE &gt; &gt; &gt;',['../classoriginal_1_1chain.html',1,'original']]],
-  ['chain_3c_20std_3a_3ashared_5fptr_3c_20original_3a_3atransform_3c_20type_20_3e_20_3e_20_3e_4',['chain&lt; std::shared_ptr&lt; original::transform&lt; TYPE &gt; &gt; &gt;',['../classoriginal_1_1chain.html',1,'original']]],
-  ['cloneable_5',['cloneable',['../classoriginal_1_1cloneable.html',1,'original']]],
-  ['comparator_6',['comparator',['../classoriginal_1_1comparator.html',1,'original']]],
-  ['container_7',['container',['../classoriginal_1_1container.html',1,'original']]],
-  ['container_3c_20bool_20_3e_8',['container&lt; bool &gt;',['../classoriginal_1_1container.html',1,'original']]],
-  ['container_3c_20opts_20_3e_9',['container&lt; opts &gt;',['../classoriginal_1_1container.html',1,'original']]],
-  ['container_3c_20underlying_5ftype_20_3e_10',['container&lt; underlying_type &gt;',['../classoriginal_1_1container.html',1,'original']]],
-  ['copytransform_11',['copyTransform',['../classoriginal_1_1copy_transform.html',1,'original']]],
-  ['couple_12',['couple',['../classoriginal_1_1couple.html',1,'original']]]
+  ['callbackreturntypeerror_1',['CallbackReturnTypeError',['../classoriginal_1_1_callback_return_type_error.html',1,'original']]],
+  ['callbacksignatureerror_2',['CallbackSignatureError',['../classoriginal_1_1_callback_signature_error.html',1,'original']]],
+  ['chain_3',['chain',['../classoriginal_1_1chain.html',1,'original']]],
+  ['chain_3c_20opts_20_3e_4',['chain&lt; opts &gt;',['../classoriginal_1_1chain.html',1,'original']]],
+  ['chain_3c_20std_3a_3ashared_5fptr_3c_20original_3a_3afilter_3c_20type_20_3e_20_3e_20_3e_5',['chain&lt; std::shared_ptr&lt; original::filter&lt; TYPE &gt; &gt; &gt;',['../classoriginal_1_1chain.html',1,'original']]],
+  ['chain_3c_20std_3a_3ashared_5fptr_3c_20original_3a_3atransform_3c_20type_20_3e_20_3e_20_3e_6',['chain&lt; std::shared_ptr&lt; original::transform&lt; TYPE &gt; &gt; &gt;',['../classoriginal_1_1chain.html',1,'original']]],
+  ['cloneable_7',['cloneable',['../classoriginal_1_1cloneable.html',1,'original']]],
+  ['comparator_8',['comparator',['../classoriginal_1_1comparator.html',1,'original']]],
+  ['container_9',['container',['../classoriginal_1_1container.html',1,'original']]],
+  ['container_3c_20bool_20_3e_10',['container&lt; bool &gt;',['../classoriginal_1_1container.html',1,'original']]],
+  ['container_3c_20opts_20_3e_11',['container&lt; opts &gt;',['../classoriginal_1_1container.html',1,'original']]],
+  ['container_3c_20underlying_5ftype_20_3e_12',['container&lt; underlying_type &gt;',['../classoriginal_1_1container.html',1,'original']]],
+  ['copytransform_13',['copyTransform',['../classoriginal_1_1copy_transform.html',1,'original']]],
+  ['couple_14',['couple',['../classoriginal_1_1couple.html',1,'original']]]
 ];
