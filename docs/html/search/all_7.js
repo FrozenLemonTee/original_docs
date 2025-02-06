17e21325a24fd4b84fab56d4a6fde3deb13362b9
frozenlemontee <frozenlemontee@gmail.com>
2025-02-06 23:38:54 +0800

    Update docs

--- docs/html/search/all_7.js
+++ docs/html/search/all_7.js
@@ -8,10 +8,15 @@ var searchData=
   ['iterable_3c_20underlying_5ftype_20_3e_5',['iterable&lt; underlying_type &gt;',['../classoriginal_1_1iterable.html',1,'original']]],
   ['iteradaptor_6',['iterAdaptor',['../classoriginal_1_1iterable_1_1iterAdaptor.html',1,'original::iterable']]],
   ['iterationstream_7',['iterationStream',['../classoriginal_1_1iterationStream.html',1,'original']]],
-  ['iterationstream_3c_20bool_20_3e_8',['iterationStream&lt; bool &gt;',['../classoriginal_1_1iterationStream.html',1,'original']]],
-  ['iterationstream_3c_20opts_20_3e_9',['iterationStream&lt; opts &gt;',['../classoriginal_1_1iterationStream.html',1,'original']]],
-  ['iterationstream_3c_20underlying_5ftype_20_3e_10',['iterationStream&lt; underlying_type &gt;',['../classoriginal_1_1iterationStream.html',1,'original']]],
-  ['iterator_11',['Iterator',['../classoriginal_1_1array_1_1Iterator.html',1,'original::array&lt; TYPE &gt;::Iterator'],['../classoriginal_1_1bitSet_1_1Iterator.html',1,'original::bitSet::Iterator'],['../classoriginal_1_1blocksList_1_1Iterator.html',1,'original::blocksList&lt; TYPE &gt;::Iterator'],['../classoriginal_1_1chain_1_1Iterator.html',1,'original::chain&lt; TYPE &gt;::Iterator'],['../classoriginal_1_1forwardChain_1_1Iterator.html',1,'original::forwardChain&lt; TYPE &gt;::Iterator'],['../classoriginal_1_1vector_1_1Iterator.html',1,'original::vector&lt; TYPE &gt;::Iterator']]],
-  ['iterator_12',['iterator',['../classoriginal_1_1iterator.html',1,'original']]],
-  ['iterator_3c_20bool_20_3e_13',['iterator&lt; bool &gt;',['../classoriginal_1_1iterator.html',1,'original']]]
+  ['iterationstream_3c_20bool_2c_20bitset_20_3e_8',['iterationStream&lt; bool, bitSet &gt;',['../classoriginal_1_1iterationStream.html',1,'original']]],
+  ['iterationstream_3c_20opts_2c_20chain_3c_20opts_20_3e_20_3e_9',['iterationStream&lt; opts, chain&lt; opts &gt; &gt;',['../classoriginal_1_1iterationStream.html',1,'original']]],
+  ['iterationstream_3c_20type_2c_20array_3c_20type_20_3e_20_3e_10',['iterationStream&lt; TYPE, array&lt; TYPE &gt; &gt;',['../classoriginal_1_1iterationStream.html',1,'original']]],
+  ['iterationstream_3c_20type_2c_20blockslist_3c_20type_20_3e_20_3e_11',['iterationStream&lt; TYPE, blocksList&lt; TYPE &gt; &gt;',['../classoriginal_1_1iterationStream.html',1,'original']]],
+  ['iterationstream_3c_20type_2c_20chain_3c_20type_20_3e_20_3e_12',['iterationStream&lt; TYPE, chain&lt; TYPE &gt; &gt;',['../classoriginal_1_1iterationStream.html',1,'original']]],
+  ['iterationstream_3c_20type_2c_20forwardchain_3c_20type_20_3e_20_3e_13',['iterationStream&lt; TYPE, forwardChain&lt; TYPE &gt; &gt;',['../classoriginal_1_1iterationStream.html',1,'original']]],
+  ['iterationstream_3c_20type_2c_20vector_3c_20type_20_3e_20_3e_14',['iterationStream&lt; TYPE, vector&lt; TYPE &gt; &gt;',['../classoriginal_1_1iterationStream.html',1,'original']]],
+  ['iterationstream_3c_20underlying_5ftype_2c_20array_3c_20underlying_5ftype_20_3e_20_3e_15',['iterationStream&lt; underlying_type, array&lt; underlying_type &gt; &gt;',['../classoriginal_1_1iterationStream.html',1,'original']]],
+  ['iterator_16',['Iterator',['../classoriginal_1_1array_1_1Iterator.html',1,'original::array&lt; TYPE &gt;::Iterator'],['../classoriginal_1_1bitSet_1_1Iterator.html',1,'original::bitSet::Iterator'],['../classoriginal_1_1blocksList_1_1Iterator.html',1,'original::blocksList&lt; TYPE &gt;::Iterator'],['../classoriginal_1_1chain_1_1Iterator.html',1,'original::chain&lt; TYPE &gt;::Iterator'],['../classoriginal_1_1forwardChain_1_1Iterator.html',1,'original::forwardChain&lt; TYPE &gt;::Iterator'],['../classoriginal_1_1vector_1_1Iterator.html',1,'original::vector&lt; TYPE &gt;::Iterator']]],
+  ['iterator_17',['iterator',['../classoriginal_1_1iterator.html',1,'original']]],
+  ['iterator_3c_20bool_20_3e_18',['iterator&lt; bool &gt;',['../classoriginal_1_1iterator.html',1,'original']]]
 ];
