8e2d29d1f3fb169ba341c436f7c637e4625f5dcf
frozenlemontee <frozenlemontee@gmail.com>
2025-04-03 21:04:33 +0800

    Update docs

--- docs/html/search/typedefs_1.js
+++ docs/html/search/typedefs_1.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['propagate_5fon_5fcontainer_5fcopy_5fassignment_0',['propagate_on_container_copy_assignment',['../classoriginal_1_1allocatorBase.html#a4482f11b43c56841b92c540bf7cb1c1f',1,'original::allocatorBase']]],
-  ['propagate_5fon_5fcontainer_5fmove_5fassignment_1',['propagate_on_container_move_assignment',['../classoriginal_1_1allocatorBase.html#a7c7dfeface4ec23346f30205a27c4dda',1,'original::allocatorBase::propagate_on_container_move_assignment'],['../classoriginal_1_1objPoolAllocator.html#a5e66fe52eddc58fdc5e37764d94b84d9',1,'original::objPoolAllocator::propagate_on_container_move_assignment']]]
+  ['propagate_5fon_5fcontainer_5fmove_5fassignment_1',['propagate_on_container_move_assignment',['../classoriginal_1_1allocatorBase.html#a7c7dfeface4ec23346f30205a27c4dda',1,'original::allocatorBase::propagate_on_container_move_assignment'],['../classoriginal_1_1objPoolAllocator.html#a5e66fe52eddc58fdc5e37764d94b84d9',1,'original::objPoolAllocator::propagate_on_container_move_assignment']]],
+  ['propagate_5fon_5fcontainer_5fswap_2',['propagate_on_container_swap',['../classoriginal_1_1allocatorBase.html#a1a901efeff0b73b8505b9d53b9d92cd8',1,'original::allocatorBase::propagate_on_container_swap'],['../classoriginal_1_1objPoolAllocator.html#a52f7225d3389a0d6bc21b5338f1046c2',1,'original::objPoolAllocator::propagate_on_container_swap']]]
 ];
