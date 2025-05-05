5e64cfa15f229cc8ecbaae92288e4c3a7410c600
frozenlemontee <frozenlemontee@gmail.com>
2025-05-05 16:03:33 +0800

    docs: Update docs

--- docs/html/search/classes_f.js
+++ docs/html/search/classes_f.js
@@ -4,9 +4,11 @@ var searchData=
   ['rangefilter_1',['rangeFilter',['../classoriginal_1_1rangeFilter.html',1,'original']]],
   ['rbnode_2',['RBNode',['../classoriginal_1_1RBTree_1_1RBNode.html',1,'original::RBTree']]],
   ['rbtree_3',['RBTree',['../classoriginal_1_1RBTree.html',1,'original']]],
-  ['refcntptr_4',['refCntPtr',['../classoriginal_1_1refCntPtr.html',1,'original']]],
-  ['refcntptr_3c_20type_2c_20strongptr_3c_20type_2c_20deleter_20_3e_2c_20deleter_20_3e_5',['refCntPtr&lt; TYPE, strongPtr&lt; TYPE, DELETER &gt;, DELETER &gt;',['../classoriginal_1_1refCntPtr.html',1,'original']]],
-  ['refcntptr_3c_20type_2c_20weakptr_3c_20type_2c_20deleter_20_3e_2c_20deleter_20_3e_6',['refCntPtr&lt; TYPE, weakPtr&lt; TYPE, DELETER &gt;, DELETER &gt;',['../classoriginal_1_1refCntPtr.html',1,'original']]],
-  ['refcount_7',['refCount',['../classoriginal_1_1refCount.html',1,'original']]],
-  ['refcount_3c_20type_2c_20deleter_3c_20type_20_3e_20_3e_8',['refCount&lt; TYPE, deleter&lt; TYPE &gt; &gt;',['../classoriginal_1_1refCount.html',1,'original']]]
+  ['rbtree_3c_20k_5ftype_2c_20v_5ftype_2c_20alloc_2c_20compare_20_3e_4',['RBTree&lt; K_TYPE, V_TYPE, ALLOC, Compare &gt;',['../classoriginal_1_1RBTree.html',1,'original']]],
+  ['rbtree_3c_20type_2c_20const_20bool_2c_20alloc_2c_20compare_20_3e_5',['RBTree&lt; TYPE, const bool, ALLOC, Compare &gt;',['../classoriginal_1_1RBTree.html',1,'original']]],
+  ['refcntptr_6',['refCntPtr',['../classoriginal_1_1refCntPtr.html',1,'original']]],
+  ['refcntptr_3c_20type_2c_20strongptr_3c_20type_2c_20deleter_20_3e_2c_20deleter_20_3e_7',['refCntPtr&lt; TYPE, strongPtr&lt; TYPE, DELETER &gt;, DELETER &gt;',['../classoriginal_1_1refCntPtr.html',1,'original']]],
+  ['refcntptr_3c_20type_2c_20weakptr_3c_20type_2c_20deleter_20_3e_2c_20deleter_20_3e_8',['refCntPtr&lt; TYPE, weakPtr&lt; TYPE, DELETER &gt;, DELETER &gt;',['../classoriginal_1_1refCntPtr.html',1,'original']]],
+  ['refcount_9',['refCount',['../classoriginal_1_1refCount.html',1,'original']]],
+  ['refcount_3c_20type_2c_20deleter_3c_20type_20_3e_20_3e_10',['refCount&lt; TYPE, deleter&lt; TYPE &gt; &gt;',['../classoriginal_1_1refCount.html',1,'original']]]
 ];
